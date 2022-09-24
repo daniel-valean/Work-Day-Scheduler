0bd27f5f0c9d59d7ef8b9efd0944b85597ca20be
Danny Valean <dannyvalean@Dannys-MacBook-Pro.local>
2022-09-23 19:03:58 -0700

    if else

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,9 +3,31 @@ $("#currentDay").html(todayDate);
 
 $(document.ready(
     function () {
-    $(".saveBtn").on("click", function (){
-        var text = $(this).siblings(".description").val();
-        var time = $
+        $(".saveBtn").on("click", function () {
+            var text = $(this).siblings(".description").val();
+            var timeblockid = $(this).parent().attr("id");
+            
+            localStorage.setItem(timeblockid, text);})
+        
+        function timeTracker() {
+        
+        $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+        
+        if (blockTime < timeNow) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+        else if (blockTime === timeNow) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
     })
-    }
-)
+})
